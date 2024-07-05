1fefb274ec8eaf21cea9750f558bb932509a7c32
Shantanu <dekateshantanu015@gmail.com>
2024-07-05 23:30:35 +0530

    Created Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Top from "./components/Header";
+import Top from "./components/Top";
 import "./App.css";
 import Puzzle from "./components/Puzzle";
+import Bottom from "./components/Bottom";
 
 function App() {
   return (
     <>
       <Top />
       <Puzzle />
+      <Bottom />
     </>
   );
 }
---
+++ src/components/Bottom.jsx
@@ -0,0 +1,17 @@
+function Bottom() {
+  return (
+    <div style={{ marginTop: "36px" }}>
+      <p>
+        Created By:{" "}
+        <a
+          target="_blank"
+          href="https://github.com/dekateshantanu015/where-is-weirdo"
+        >
+          Shantanu Dekate
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Bottom;
